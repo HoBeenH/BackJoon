f7ac037214418f3ddfa92760303acaec04a6e577
X1 Carbon <X1 Carbon@LAPTOP-KLH1RVSV>
2021-08-02 19:47:26 +0900

    다됨초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BackJoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string a = "안뇽~";
+        }
+    }
+}
