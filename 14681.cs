81c37d085c9195b12930e0cc48573a0568404c23
호빈 황 <whdmstlsk@gmail.com>
2021-08-03 10:25:32 +0900

    14681번 문제

---
+++ 14681.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (x > 0 && y > 0)
+            {
+                Console.Write("1");
+            }
+            else if (x < 0 && y > 0)
+            {
+                Console.Write("2");
+            }
+            else if (x < 0 && y < 0)
+            {
+                Console.Write("3");
+            }
+            else if (x > 0 && y < 0)
+            {
+                Console.Write("4");
+            }
+        }
+    }
+}
