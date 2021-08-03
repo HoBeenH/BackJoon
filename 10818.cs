912817e122ab56177f0a6857fb4929898eeb992d
호빈 황 <whdmstlsk@gmail.com>
2021-08-03 10:46:26 +0900

    10818번 문제

---
+++ 10818.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BackJoon
+{
+    class BackJoon10818
+    {
+        static void Main(string[] args)
+        {
+            int numCount = int.Parse(Console.ReadLine());
+            string[] temp = Console.ReadLine().Split(" ");
+            int[] num = new int[temp.Length];
+            for (int i = 0; i < numCount; i++)
+            {
+                num[i] = int.Parse(temp[i]);
+            }
+            int min = num[0];
+            int max = num[0];
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (min > num[i])
+                {
+                    min = num[i];
+                }
+                if (max < num[i])
+                {
+                    max = num[i];
+                }
+            }
+            Console.Write($"{min} {max}");
+        }
+
+    }
+}
