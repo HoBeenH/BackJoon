c47c75cb1b0b61407ad61a2e3117f8d569f22043
호빈 황 <whdmstlsk@gmail.com>
2021-08-03 10:32:59 +0900

    11654번 문제

---
+++ 11654.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+class BackJoon11654
+{
+    static void Main()
+    {
+        char[] temp = Console.ReadLine().ToCharArray();
+        int a = temp[0];
+        Console.Write(a);
+    }
+}
