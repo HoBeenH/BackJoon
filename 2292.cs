cd52a5a9a985c15c566d642a42cab68f05811ce7
호빈 황 <whdmstlsk@gmail.com>
2021-08-08 14:06:08 +0900

    2292

---
+++ 2292.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BackJoon
+{
+    class BackJoon2292
+    {
+        static void Quest()
+        {
+            int findRoomNumber = int.Parse(Console.ReadLine());
+            int firstRoom = 1;
+            int fixRoom = 6;
+            int result;
+            if (findRoomNumber == 1)
+            {
+                result = 1;
+                Console.Write(result);
+            }
+            else
+            {
+                for (int i = 1; ; i++)
+                {
+                    if (firstRoom < findRoomNumber && findRoomNumber <= firstRoom + (fixRoom * i))
+                    {
+                        result = i + 1;
+                        Console.Write(result);
+                        return;
+                    }
+                    firstRoom += fixRoom * i;
+                }
+            }
+        }
+    }
+}
