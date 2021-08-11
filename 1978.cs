b7e8dd48a2420a0e45560cd5302bb425f78e7a60
HoBeenH <whdmstlsk@gmail.com>
2021-08-11 10:43:47 +0900

    1978

---
+++ 1978.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class BackJoon1978
+{
+    static void Quest()
+    {
+        int num = int.Parse(Console.ReadLine());
+        int count = 0;
+        string[] inputNum = Console.ReadLine().Split();
+        for (int k = 0; k < num; k++)
+        {
+            int input = int.Parse(inputNum[k]);
+            if (input == 1)
+            {
+                continue;
+            }
+            bool temp = true;
+            for (int j = 2; j <= MathF.Sqrt(input); j++)
+            {
+                if (input % j == 0)
+                {
+                    temp = false;
+                    break;
+                }
+            }
+            if (temp)
+            {
+                count += 1;
+            }
+        }
+        Console.Write(count);
+    }
+}
