79714cf380e706f5c39d7d5e005f894d25e9ec0a
HoBeenH <whdmstlsk@gmail.com>
2021-08-17 16:27:32 +0900

    1085

---
+++ 1085.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class BackJoon1085
+{
+    static void Quest()
+    {
+        string[] temp = Console.ReadLine().Split(" ");
+        int nowX = int.Parse(temp[0]);
+        int nowY = int.Parse(temp[1]);
+        int maxX = int.Parse(temp[2]);
+        int maxY = int.Parse(temp[3]);
+
+        int disX = maxX - nowX > nowX ? nowX : maxX - nowX;
+        int disY = maxY - nowY > nowY ? nowY : maxY - nowY;
+        int resutl = disX > disY ? disY : disX;
+        Console.Write(resutl);
+    }
+}
