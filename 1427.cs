008b2b680ba3d5f0bb8c73c54515707f453836ba
HoBeenH <87354491+HoBeenH@users.noreply.github.com>
2021-10-18 20:18:39 +0900

    Create 1427.cs

---
+++ 1427.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var nums = (Console.ReadLine().ToCharArray().Select(num => num - 48).OrderBy(i => i).ToArray());
+            var result = new StringBuilder();
+            for (var i = nums.Length - 1; i >= 0; i--)
+            {
+                result.Append(nums[i]);
+            }
+
+            Console.Write(result);
+        }
+    }
+}
