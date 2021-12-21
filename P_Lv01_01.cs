d3944bc94b7efd0caedaf1d2b2c22fd6ee71855f
HoBeenH <87354491+HoBeenH@users.noreply.github.com>
2021-12-21 13:44:21 +0900

    k

---
+++ P_Lv01_01.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace Programmers
+{
+    public class Test
+    {
+        public static void Main(string[] args)
+        {
+            var _lotto = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var _winNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var _result = solution(_lotto, _winNums);
+            foreach (var VARIABLE in _result)
+            {
+                Console.Write(VARIABLE);
+            }
+        }
+
+        public static int[] solution(int[] lotto, int[] win_nums)
+        {
+            int _zeroCnt = 0, _resultCnt = 0;
+            foreach (var t in lotto)
+            {
+                if (t == 0)
+                {
+                    _zeroCnt += 1;
+                    continue;
+                }
+
+                if (win_nums.Any(t1 => t == t1))
+                {
+                    _resultCnt += 1;
+                }
+            }
+
+            return new int[] {FindWinner(_resultCnt + _zeroCnt), FindWinner(_resultCnt)};
+        }
+
+        private static int FindWinner(int num)
+        {
+            var _return = 0;
+            switch (num)
+            {
+                case 0:
+                    _return = 6;
+                    break;
+                case 1:
+                    _return = 6;
+                    break;
+                case 2:
+                    _return = 5;
+                    break;
+                case 3:
+                    _return = 4;
+                    break;
+                case 4:
+                    _return = 3;
+                    break;
+                case 5:
+                    _return = 2;
+                    break;
+                case 6:
+                    _return = 1;
+                    break;
+            }
+
+            return _return;
+        }
+        //1 = 1등
+        //2 = 5개
+        //3 = 4
+        //4 = 3
+        //5 = 2
+        //6 = 탈락
+    }
+}
---
+++ P_Lv02_01.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Programmers
+{
+    public class Test
+    {
+        public static void Main(string[] args)
+        {
+            var _progresses = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var _speeds = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var _tmp = solution(_progresses, _speeds);
+            foreach (var VARIABLE in _tmp)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+        }
+
+        public static int[] solution(int[] progresses, int[] speeds)
+        {
+            var _result = new NewQueue(progresses, speeds);
+            while (!_result.IsFinish())
+            {
+                _result.NextDay();
+            }
+
+            return _result.Finish();
+        }
+
+        private class NewQueue
+        {
+            private readonly int[] progresses;
+            private readonly int[] speeds;
+            private readonly List<int> _return;
+
+            public NewQueue(int[] progresses, int[] speeds)
+            {
+                this.progresses = progresses;
+                this.speeds = speeds;
+                _return = new List<int>();
+            }
+
+            public void NextDay()
+            {
+                for (var i = 0; i < progresses.Length; i++)
+                {
+                    if (progresses[i] != -1)
+                    {
+                        progresses[i] += speeds[i];
+                    }
+                }
+            }
+
+            public bool IsFinish()
+            {
+                var _result = 0;
+                var _finish = false;
+                for (var i = 0; i < progresses.Length; i++)
+                {
+                    if (progresses[i] == -1)
+                    {
+                        continue;
+                    }
+
+                    if (progresses[i] >= 100)
+                    {
+                        _result += 1;
+                        progresses[i] = -1;
+                        _finish = true;
+                    }
+                    else
+                    {
+                        _finish = false;
+                        break;
+                    }
+                }
+
+                if (_result != 0)
+                {
+                    _return.Add(_result);
+                }
+
+                return _finish;
+            }
+
+            public int[] Finish()
+            {
+                return _return.ToArray();
+            }
+        }
+    }
+}
